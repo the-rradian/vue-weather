a46a90daf8ff504d6a818c1639751493fceb4319
feno <rand0085@algonquinlive.com>
2023-06-28 22:42:56 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import WeatherCard from './components/WeatherCard.vue'
+
+</script>
+
+<template>
+
+  <main>
+    <WeatherCard/>
+    <footer>Search icon by <a target="_blank" href="https://icons8.com/icon/132/search">Icons8</a></footer>
+    
+  </main>
+</template>
+
+<style scoped>
+body{
+  display: flex;
+  align-items:center;
+  justify-content: center;
+}
+footer{
+  margin-top: 100px;
+  font-size: x-small;
+}
+</style>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,177 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+
+const apiKey = '91740b6b069953b841b91f7a98eee1ce'
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+const cityURL = 'http://api.openweathermap.org/geo/1.0/direct?'
+const mode = "metric"
+const tempUnit = ref("°C")
+const selectedCity = ref("")
+const weatherData = ref({
+
+    cityName: "",
+    countryName: "",
+    weatherDescription: "",
+    currentTemp: 0,
+    minTemp: 0,
+    maxTemp: 0, 
+    feelsLike: 0,
+    humidity: 0,
+    precipitation: 0
+
+})
+
+const searchBarIsEmpty = computed(() => {
+  return selectedCity.value.trim().length === 0 ? true : false
+})
+
+async function weatherRequest(){
+
+    const coordinates = await cityRequest()
+    const apiResponse = await fetch(`${weatherURL}?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=${mode}`)
+    const apiData = await apiResponse.json()
+    console.log(apiData)
+    //weather desc 
+    let weatherText = apiData["weather"][0]["description"]
+    weatherData.value.weatherDescription = capitalizeFirstLetter(weatherText)
+    //temperature in Celsius and Fahrenheit
+    weatherData.value.currentTemp = Math.floor(apiData["main"]["temp"])  
+    weatherData.value.feelsLike = Math.floor(apiData["main"]["feels_like"])
+    //everything else
+    weatherData.value.humidity = apiData["main"]["humidity"]
+    weatherData.value.minTemp = apiData["main"]["temp_min"]
+    weatherData.value.maxTempTemp = apiData["main"]["temp_max"]
+
+}
+
+async function cityRequest(){
+    //hit city API for the city in search bar
+    const apiResponse = await fetch(`${cityURL}q=${selectedCity.value.trim()}&limit=3&appid=${apiKey}`)
+
+    const apiData = await apiResponse.json()
+    //if city is found, apiData should be populated
+    if (apiData){
+        //grab city and country code
+        weatherData.value.cityName = apiData["0"]["name"]
+        weatherData.value.countryName = apiData["0"]["country"]
+        //use the coordinates of the first element in the JSON
+        const coordinates = {
+            latitude : apiData["0"]["lat"],
+            longitude : apiData["0"]["lon"]
+        }
+        return coordinates
+    } else
+    console.log("City not found")
+
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function modeSwitch() {
+    if (mode === "metric"){
+        mode = "imperial"
+        tempUnit.value = "°F"
+        //change all temps to °F
+    }
+    if (mode === "imperial"){
+        mode = "metric"
+        tempUnit.value = "°C"
+        //change all temps to °C 
+        // 
+    }
+}
+
+function convertToFahrenheit(temp){
+    return (temp*1.8) + 32
+}
+
+function convertToCelsius(temp){
+    return (temp-32)/1.8 
+}
+
+</script>
+
+<template >
+    <div class="search">
+        <input v-model="selectedCity" class="search-bar" type="text" placeholder="Enter city name" title="Enter city name"/>
+        <button class="button" @click="weatherRequest()" v-bind:disabled="searchBarIsEmpty" title="Search">
+            <img id="search-icon" src="../assets/icons8-search-50.png"/>
+        </button>
+    </div>
+    <img src="../assets/23527-3-weather.png" width="250" height="250"/>
+    <h1 class="city">Weather in <a id="city-name">{{ weatherData.cityName }}, {{ weatherData.countryName }}</a></h1>
+    <div v-if="weatherData" class="conditions">
+        <h1 id="temperature">{{ weatherData.currentTemp }} {{tempUnit}}</h1>
+        <h3 id="feels-like">Feels like: {{ weatherData.feelsLike }} {{tempUnit}}</h3>
+    </div>
+    <h1 class="conditions" id="current-weather">{{weatherData.weatherDescription}}</h1>
+        <div>
+            <h2 id="humidity">Humidity: <h3>{{weatherData.humidity}}%</h3></h2>
+        </div>
+
+</template>
+
+<style scoped>
+
+.search-bar{
+    height: 40px;
+    margin: 10px;
+    padding: 15px;
+    border: none;
+    border-radius: 25px;
+    box-shadow: 2px 2px 1px rgb(80, 80, 80);
+}
+
+.button {
+    width: 43px;
+    height: 43px;
+    margin: 10px;
+    border: none;
+    border-radius: 50%;
+    box-shadow: 2px 2px 1px rgb(80, 80, 80);
+    
+}
+.conditions{
+    display: grid;
+}
+
+.precipitation{
+    margin-left: 10px;
+}
+
+#feels-like {
+    font-size: small;
+}
+
+#search-icon{
+    width: 16px;
+    height: 16px;
+}
+
+#temperature{
+    font-weight: bolder;
+    padding-top: 20px;
+    padding-bottom: 5px;
+}
+#current-weather{
+
+    font-weight:500;
+
+}
+#humidity{
+
+    margin-right: 10px;
+
+}
+
+#precipiation{
+
+    margin-right: 10px;
+
+
+}
+
+
+</style>
